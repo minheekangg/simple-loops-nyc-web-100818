1abe4ffd9999d8caf498b23590d66cb71a7ebdf8
Min Hee  Park <minheekangg@gmail.com>
2018-09-22 21:11:25 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -10,7 +10,7 @@ def loop_iterator(number_of_times)
   #  phrase and break when the counter hits the appropriate number...
 
 phrase = "Welcome to Flatiron School's Web Development Course!"
-count = 1
+count = 0
 loop do
     puts phrase
     count +=1
